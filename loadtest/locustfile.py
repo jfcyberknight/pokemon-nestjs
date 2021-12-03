30724cb0d2bc754510ffe1cdfac6e3b114790290
Vallée Jean-François <vallee.jeanfrancois@gmail.com>
2021-12-02 21:05:14 -0500

    add put and post in loadtest

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,9 +1,13 @@
 from locust import HttpUser, task
 from locust.env import Environment
-from requests.structures import CaseInsensitiveDict
 from random import randint
+from requests.structures import CaseInsensitiveDict
 
-URL = "http://host.docker.internal:3000"
+import random
+import string
+
+PORT = "5000"
+URL = f'http://host.docker.internal:{PORT}'
 EMAIL = "<replace>"
 PASSWORD = "<replace>"
 AUTHENTICATION_TOKEN = "<replace>"
@@ -56,6 +60,73 @@ class User(HttpUser):
         with self.client.get(f'{URL}/pokemons/{pokemonId}',
                              headers=authorization_headers,
                              catch_response=True) as response:
+            responseFormatter(response, context)
+
+    @ task
+    def pokemon_api_post_pokemons(self):
+        postPokemonId = 800
+        context = "pokemon_api_post_pokemons"
+
+        name = string.ascii_lowercase
+        name = ''.join(random.choice(name) for i in range(10))
+
+        type1 = string.ascii_lowercase
+        type1 = ''.join(random.choice(type1) for i in range(10))
+
+        type2 = string.ascii_lowercase
+        type2 = ''.join(random.choice(type2) for i in range(10))
+
+        postPokemonId += 1
+
+        with self.client.post(f'{URL}/pokemons',
+                              json={
+                                  "Id": postPokemonId,
+                                  "Name": name,
+                                  "Type1": type1,
+                                  "Type2": type2,
+                                  "Total": randint(1, 1000),
+                                  "Hp": randint(1, 1000),
+                                  "Attack": randint(1, 1000),
+                                  "Defense": randint(1, 1000),
+                                  "SpAtk": randint(1, 1000),
+                                  "SpDef": randint(1, 1000),
+                                  "Speed": randint(1, 1000),
+                                  "Generation": randint(1, 1000),
+                                  "Legendary": 'true'
+                              },
+                              headers=authorization_headers,
+                              catch_response=True) as response:
+            responseFormatter(response, context)
+
+    @ task
+    def pokemon_api_patch_pokemons(self):
+        context = "pokemon_api_patch_pokemons"
+        name = string.ascii_lowercase
+        name = ''.join(random.choice(name) for i in range(10))
+
+        type1 = string.ascii_lowercase
+        type1 = ''.join(random.choice(type1) for i in range(10))
+
+        type2 = string.ascii_lowercase
+        type2 = ''.join(random.choice(type2) for i in range(10))
+
+        with self.client.patch(f'{URL}/pokemons/2',
+                               json={
+                                   "Name": name,
+                                   "Type1": type1,
+                                   "Type2": type2,
+                                   "Total": randint(1, 1000),
+                                   "Hp": randint(1, 1000),
+                                   "Attack": randint(1, 1000),
+                                   "Defense": randint(1, 1000),
+                                   "SpAtk": randint(1, 1000),
+                                   "SpDef": randint(1, 1000),
+                                   "Speed": randint(1, 1000),
+                                   "Generation": randint(1, 1000),
+                                   "Legendary": 'true'
+                               },
+                               headers=authorization_headers,
+                               catch_response=True) as response:
             responseFormatter(response, context)
 
 
